9660d2153952c86edd2cabf26bb24876e4246e52
GustavoRobertoLarenasCastilla <gustavo.roberto.larenas.castilla@gmail.com>
2016-05-16 02:15:33 -0500

    Initial commit

---
+++ app/src/main/java/elicom/com/bienvenido/MainActivity.java
@@ -0,0 +1,14 @@
+package elicom.com.bienvenido;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(getWindow().FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+    }
+}
